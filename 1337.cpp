ab1de70a60a22ce8923fac3d37ee39d46f55f3c2
Fausto Pinto Cabrera <83981597+Fausto1712@users.noreply.github.com>
2022-10-20 19:12:30 -0500

    1337

---
+++ 1337.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+typedef pair<int, int> pi;
+
+class Solution {
+    typedef pair<int, int> pi;
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pi, vector<pi>, greater<pi> > analize;
+        vector<int> solution;
+        int value;
+        for (int i = 0; i < mat.size(); i++)
+        {
+            value = 0;
+            for (int j = 0; j < mat[i].size(); j++)
+            {
+                if (mat[i][j] == 1)
+                {
+                    value++;
+                }
+            }
+            analize.push(make_pair(value,i));
+        }
+        for (int i = 0; i < k; i++)
+        {
+            solution.push_back(analize.top().second);
+            analize.pop();
+        }
+        return solution;
+    }
+};
