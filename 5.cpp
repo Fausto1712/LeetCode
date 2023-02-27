316e1cd78c8704f5f2575d8bcce70cef0b682fc7
Fausto Pinto Cabrera <83981597+Fausto1712@users.noreply.github.com>
2023-02-26 21:57:42 -0600

    5

--- 5.cpp
+++ 5.cpp
@@ -5,61 +5,45 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int start = 0,end = 0,size = 0,x = 0;
-        for (int i = 0; i < s.length(); i++)
+        string longest;
+        int st,size, max = 0,left,right;
+        for (int i = 0; i < s.size(); i++)
         {
-            if (s[i] == s[i-1] || s[i] == s[i+1])
+            int x = 1;
+            if (s[i] == s[i + 1])
             {
                 size = 2;
-                x = 1;
-                if (s[i] == s[i-1])
-                {
-                    while (s[i+x] == s[i-x-1])
-                    {
-                        size = size + 2;
-                        x++;
-                    }
-                    end = i+x;
-                    start = i-x-1;
-                } else if (s[i] == s[i+1])
-                {
-                    while (s[i-x] == s[i+x+1])
-                    {
-                        size = size + 2;
-                        x++;
-                    }
-                    start = i-x;
-                    end = i+x+1;
-                }
-            }
-            else if (s[i-1] == s[i+1])
-            {
-                size = 3;
-                x = 2;
-                while (s[i-x] == s[i+x])
+                while (((i-x) >= 0) && ((i+x) <= s.size()) && (s[i - x] == s[i + 1 + x]))
                 {
                     size = size + 2;
                     x++;
                 }
-                start = i-x;
-                end = i+x;
+                if (size > max)
+                {
+                    max = size;
+                    st = i+1-x;
+                }
+            }
+            size = 1;
+            x = 1;
+            while (((i-x) >= 0) && ((i+x) <= s.size()) && (s[i - x] == s[i + x]))
+            {
+                size = size + 2;
+                x++;
+            }
+            if (size > max)
+            {
+                max = size;
+                st = i+1-x;
             }
         }
-
-        string result;
-        while (start <= end)
-        {
-            result = result + s[start];
-            start++;
-        }
-        cout << result << endl;
-        return result;
+        return s.substr(st, max);
     }
 };
 
 int main(){
-    string test =  "akaaluhhula";
+    string test =  "aaaa";
     //cin >> test;
     Solution s1;
-    s1.longestPalindrome(test);
+    cout << s1.longestPalindrome(test);
 }
